b19e226191d3ccb1d70c416bcf4b02d59d77a702
sktudublin <D18124684@mytudublin.ie>
2019-11-01 20:29:55 +0000

    tracking

---
+++ grinds-ui/src/app/validator.ts
@@ -0,0 +1,19 @@
+import { AbstractControl } from '@angular/forms';
+
+export function passwordValidator(control: AbstractControl) {
+    if (control && (control.value !== null || control.value !== undefined)) {
+        const confirmPassword = control.value;
+
+        const passControl = control.root.get('password'); 
+        if (passControl) {
+            const passValue = passControl.value;
+            if (passValue !== confirmPassword || passValue === '') {
+                return {
+                    isError: true
+                };
+            }
+        }
+    }
+
+    return null;
+}
