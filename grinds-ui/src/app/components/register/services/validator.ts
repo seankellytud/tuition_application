f993147485bb0d21e08ef0b8fabd9f80070e2998
sktudublin <D18124684@mytudublin.ie>
2019-11-03 17:45:53 +0000

    Comments added to custom password check validator

--- grinds-ui/src/app/components/register/services/validator.ts
+++ grinds-ui/src/app/components/register/services/validator.ts
@@ -1,13 +1,15 @@
-import { AbstractControl, FormGroup } from '@angular/forms';
+//Sourced: https://github.com/AzharHusain/reactive-form-custom-validation/blob/master/src/app/validator.ts
+import { AbstractControl, FormGroup } from '@angular/forms'; 
+
 
 export function passwordValidator(control: AbstractControl) {
-    if (control && (control.value !== null || control.value !== undefined)) {
-        const confirmPassword = control.value;
+    if (control && (control.value !== null || control.value !== undefined)) { //set stipulations for control to move forward with fuction
+        const confirmPassword = control.value; //save control into variable
 
-        const passControl = control.root.get('password'); 
+        const passControl = control.root.get('password'); //get value that was passed into the first password field
         if (passControl) {
-            const passValue = passControl.value;
-            if (passValue !== confirmPassword || passValue === '') {
+            const passValue = passControl.value; //save that value into variable
+            if (passValue !== confirmPassword || passValue === '') { //compare the two variables
                 return {
                     isError: true
                 };
